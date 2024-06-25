4b5e2adef7f06125af61a6682f34430ac6365d9f
Toan <ubw.zeroxa@gmail.com>
2024-06-24 20:45:37 -0500

    * changed host domain

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const HOST = 'http://rem.fishy-tuna.com:3000';
 // export const HOST = 'http://localhost:3000';
-export const HOST = 'http://rem.mythzxa.com:3000';
+export const HOST = 'https://dc.mythzxa.com';
