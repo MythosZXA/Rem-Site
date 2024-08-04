cc75fe48b57b155d1c295c4bf17e3c96c3e8be57
MythosZXA <ubw.zeroxa@gmail.com>
2024-08-04 18:00:55 -0500

    * dynamic avatar (file) extension for home page

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -5,12 +5,14 @@ import './Home.css';
 export default function Home() {
   const {auth, setAuth} = useContext(AuthContext);
 
+  const fileExt = auth.avatar.startsWith('a_') ? 'gif' : 'webp';
+
   return(
     <div className="page-container active" id="containerHome">
       <div className="profile-card">
         <span
           className="profile-avatar"
-          style={{backgroundImage: `url(https://cdn.discordapp.com/avatars/${auth.id}/${auth.avatar}.gif)`}}
+          style={{backgroundImage: `url(https://cdn.discordapp.com/avatars/${auth.id}/${auth.avatar}.${fileExt})`}}
         />
       </div>
     </div>
